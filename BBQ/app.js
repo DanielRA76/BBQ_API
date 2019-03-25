b88870403821a3f8f2406827f4ec0e6bed23ed6f
DANIEL RODRIGUEZ ALFONSO <daniel.rodriguez.alfonso@gmail.com>
2019-03-25 01:40:07 +0100

    Instalamos JsonFormatter y Body-parser

--- BBQ/app.js
+++ BBQ/app.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Variables globales de template
+app.locals.titulo = 'BBQ API';
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
