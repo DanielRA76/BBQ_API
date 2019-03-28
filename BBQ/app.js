7cb540f43c711067ff1d4dda8776351ae2edfc28
DANIEL RODRIGUEZ ALFONSO <daniel.rodriguez.alfonso@gmail.com>
2019-03-28 01:50:17 +0100

    instalamos Multer

--- BBQ/app.js
+++ BBQ/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,21 +17,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//para servir ficheros estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Variables globales de template
 app.locals.titulo = 'BBQ API';
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/**
+ * Rutas de mi aplicación Web
+ */
+app.use('/',      require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- BBQ/routes/index.js
+++ BBQ/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //la vista se renderiza (calcula ) en el servidor
   res.render('index', { title: 'Express' });
 });
 
